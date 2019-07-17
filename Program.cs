4b35ffdec595cee846af43680d1ff83943a2329e
asokolov <artem.sokolov@idt.net>
2019-07-17 13:14:44 +0300

    converted to simple HTTP file server

--- Program.cs
+++ Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Idt.Test.Service
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Default options
+            var contentRoot = Directory.GetCurrentDirectory();
+            var port = 8000;
+
+            // Parse arguments
+            var remainingArgs = args.ToList();
+            while (remainingArgs.Count > 0)
+            {
+                if (Directory.Exists(remainingArgs.First()))
+                {
+                    contentRoot = remainingArgs.First();
+                }
+                else if (int.TryParse(remainingArgs.First(), out int customPort))
+                {
+                    port = customPort;
+                }
+                else
+                {
+                    break;
+                }
+
+                remainingArgs = remainingArgs.Skip(1).ToList();
+            }
+
+            var host = new WebHostBuilder()
+                .UseUrls("http://*:" + port)
+                .UseContentRoot(contentRoot)
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDirectoryBrowser();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment hostingEnvironment)
+        {
+            loggerFactory.AddConsole();
+
+            var options = new FileServerOptions()
+            {
+                FileProvider = hostingEnvironment.ContentRootFileProvider,
+                RequestPath = new PathString(string.Empty),
+                EnableDirectoryBrowsing = true,
+                EnableDefaultFiles = false
+            };
+
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+
+            app.UseFileServer(options);
         }
     }
 }
+
